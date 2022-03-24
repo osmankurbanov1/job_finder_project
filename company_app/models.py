5995dc157ec321015ea1ae8b4cfb2dea7df1332d
osmankurbanov1 <letter.to.osman@gmail.com>
2022-03-24 23:31:17 +0600

    Added 'owner' field to Company model / changed type of field from URLField to ImageField

--- company_app/models.py
+++ company_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,11 @@ class Company(models.Model):
     location = models.CharField(max_length=100)
     description = models.TextField()
     employee_count = models.IntegerField()
-    logo = models.URLField(default='https://place-hold.it/100x60')
+    #logo = models.URLField(default='https://place-hold.it/100x60')
+    logo = models.ImageField(upload_to='MEDIA_COMPANY_IMAGE_DIR', null=True)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='companies', null=True, blank=True)
 
     objects = models.Manager()
+
+    def __str__(self):
+        return self.name
