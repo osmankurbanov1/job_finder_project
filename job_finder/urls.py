7f8f606c586b7826610096381ac43a9864f45449
osmankurbanov1 <letter.to.osman@gmail.com>
2022-03-24 23:17:00 +0600

    Added condition for settings.DEBUG

--- job_finder/urls.py
+++ job_finder/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from vacancy_app import views as vacancy_views
 from company_app import views as company_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,7 @@ urlpatterns = [
     path('vacancies/', include('vacancy_app.urls')),
     path('companies/<int:id>', company_views.CompanyDetail.as_view(), name='company_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
