965ea23b2bfd4d9d62108f37ec616373137eadce
osmankurbanov1 <letter.to.osman@gmail.com>
2022-03-24 23:14:57 +0600

    Registered Specialy/Vacancy/Application models in admin

--- vacancy_app/admin.py
+++ vacancy_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Vacancy, Application, Specialty
 
 # Register your models here.
+
+
+class SpecialtyAdmin(admin.ModelAdmin):
+    pass
+
+
+class VacancyAdmin(admin.ModelAdmin):
+    pass
+    
+
+
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Vacancy, VacancyAdmin)
+admin.site.register(Application, ApplicationAdmin)
+admin.site.register(Specialty, SpecialtyAdmin)
