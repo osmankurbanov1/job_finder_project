95406459042593cdce9213c1665d8c28c9f742d8
osmankurbanov1 <letter.to.osman@gmail.com>
2022-03-24 23:29:36 +0600

    Added Application model / field 'logo' to Specialty model

--- vacancy_app/models.py
+++ vacancy_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from company_app.models import Company
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -7,9 +8,12 @@ class Specialty(models.Model):
 
     code = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='MEDIA_SPECIALITY_IMAGE_DIR', null=True)
 
     objects = models.Manager()
 
+    def __str__(self):
+        return f'{self.code} - {self.title}'
 
 class Vacancy(models.Model):
     title = models.CharField(max_length=100)
@@ -22,3 +26,19 @@ class Vacancy(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="vacancies")
 
     objects = models.Manager()
+
+    def __str__(self):
+        return self.title
+
+
+class Application(models.Model):
+    written_username = models.CharField(max_length=20)
+    written_phone = models.CharField(max_length=20)
+    written_cover_letter = models.TextField
+    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, related_name='applications')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='applications')
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f'Application from {self.written_username}'
