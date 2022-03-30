03d479549de9d7f9f6bd37da4ce25423718c2b31
osmankurbanov1 <letter.to.osman@gmail.com>
2022-03-30 20:25:04 +0600

    Added new rout and names

--- vacancy_app/urls.py
+++ vacancy_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views as vacancy_views
 
 urlpatterns = [
-    path('', vacancy_views.VacancyList.as_view()),
-    path('<int:pk>/', vacancy_views.VacancyDetail.as_view()),
-    path('cat/<str:profession>/', vacancy_views.VacancyByCategoryList.as_view()),
+    path('', vacancy_views.VacancyList.as_view(), name='vacancies_list'),
+    path('<int:pk>/', vacancy_views.VacancyDetail.as_view(), name='vacancy_detail'),
+    path('<int:pk>/send/', vacancy_views.sent),
+    path('cat/<str:profession>/', vacancy_views.VacancyByCategoryList.as_view(), name='vacancy_category'),
 ]
