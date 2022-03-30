846d73aa7d04e8d11e05f55e2df3297c9df81d15
osmankurbanov1 <letter.to.osman@gmail.com>
2022-03-30 20:20:14 +0600

    Deleted commented out code

--- company_app/models.py
+++ company_app/models.py
@@ -9,7 +9,6 @@ class Company(models.Model):
     location = models.CharField(max_length=100)
     description = models.TextField()
     employee_count = models.IntegerField()
-    #logo = models.URLField(default='https://place-hold.it/100x60')
     logo = models.ImageField(upload_to='MEDIA_COMPANY_IMAGE_DIR', null=True)
     owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='companies', null=True, blank=True)
 
