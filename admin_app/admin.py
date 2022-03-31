33283226f391ec81aa02f90daefa4b6d7afa7d3d
osmankurbanov1 <letter.to.osman@gmail.com>
2022-03-31 22:50:37 +0600

    Added Resume model to admin

--- admin_app/admin.py
+++ admin_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Resume
+
 
 # Register your models here.
+
+
+class ResumeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Resume, ResumeAdmin)
