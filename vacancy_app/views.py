2885534e61e9da10bb6ec23ec455c4b71c29cc07
osmankurbanov1 <letter.to.osman@gmail.com>
2022-04-15 12:31:46 +0600

    Added ApplicationCreateAPIView

--- vacancy_app/views.py
+++ vacancy_app/views.py
@@ -1,11 +1,73 @@
-from django.http import HttpResponseRedirect
+import datetime
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView, CreateView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, RetrieveAPIView, \
+    CreateAPIView
+
 from vacancy_app.models import Specialty, Vacancy
 from company_app.models import Company
 from .forms import AddApplication
+from .permissions import UserHasCompanyOrReadOnly, IsVacancyOwnerOrReadOnly, IsCompanyOwner
+from .serializers import VacancySerializer, SpecialtySerializer, ApplicationSerializer, VacancyRelatedCompanySerializer
 
 # Create your views here.
+
+
+class ApplicationCreateAPIView(CreateAPIView):
+    """
+    post new Application to a vacancy
+    """
+
+    serializer_class = ApplicationSerializer
+
+    def perform_create(self, serializer):
+        if self.request.user:
+            serializer.save(user_id=self.request.user.id)
+
+
+class VacancyRelatedCompanyList(ListAPIView):
+    """
+    get Vacancies related the Company
+    """
+
+    serializer_class = VacancyRelatedCompanySerializer
+    permission_classes = (IsCompanyOwner, )
+
+    def get_queryset(self):
+        return Vacancy.objects.filter(company=Company.objects.get(name=self.kwargs.get('company')))
+
+
+class VacancyListCreateAPIView(ListCreateAPIView):
+    """
+    get all Vacancies
+    post new Vacancy
+    """
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+    permission_classes = (UserHasCompanyOrReadOnly, )
+
+    def perform_create(self, serializer):
+        serializer.save(company_id=self.request.user.companies.pk, posted=datetime.date.today())
+
+
+class VacancyDetailAPIView(RetrieveUpdateDestroyAPIView):
+    """
+    read / update / delete Vacancy
+    """
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+    permission_classes = (IsVacancyOwnerOrReadOnly, )
+
+
+class SpecialtyListAPIView(ListAPIView):
+    queryset = Specialty.objects.all()
+    serializer_class = SpecialtySerializer
+
+
+class SpecialtyDetailAPIView(RetrieveAPIView):
+    queryset = Specialty.objects.all()
+    serializer_class = SpecialtySerializer
 
 
 class VacancySearch(ListView):
