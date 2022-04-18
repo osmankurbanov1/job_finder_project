e4d6430e91494da0ef4b70bf2cc7f3e7a433674d
osmankurbanov1 <letter.to.osman@gmail.com>
2022-04-18 08:15:46 +0600

    Added 3rd party auth apps

--- job_finder/settings.py
+++ job_finder/settings.py
@@ -36,13 +36,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    # Local apps
     'vacancy_app',
     'company_app',
-    'crispy_forms',
     'admin_app',
-    'rest_framework',
-    'drf_yasg',
     'api',
+    # 3rd-party apps
+    'crispy_forms',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +149,13 @@ MEDIA_SPECIALITY_IMAGE_DIR = 'speciality_images'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
